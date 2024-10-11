9aebeb9153df8b4a06376b7bb322793d974fb432
Sadil08 <sadil.ephraims2004@gmail.com>
2024-10-11 16:55:01 +0530

    Updated settings for local development and added environment variables

--- myproject/myproject/wsgi.py
+++ myproject/myproject/wsgi.py
@@ -10,7 +10,10 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
+import os
+from dotenv import load_dotenv
 
+load_dotenv()
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myproject.settings')
 
 application = get_wsgi_application()
